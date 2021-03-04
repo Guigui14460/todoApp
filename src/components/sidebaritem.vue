613af36969ea38c80428f2491e9f19ae88998efa
Arthur <arthur.bocage@gmail.com>
2021-03-04 16:09:01 +0100

    Creation des fichiers dans le dossier Component

---
+++ src/components/sidebaritem.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <input type="checkbox" name="premier" id="premier">
+    <label for="premier">
+     <input type="checkbox" name="de" id="de">
+    <label for="de">
+    <div>
+</template>
+
+
+<script>
+export default {
+  name: "Side bar item",
+  
+};
+</script>
