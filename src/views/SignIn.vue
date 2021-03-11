f74f074d3a06f63675ca93eb11919b1848471deb
Arthur <arthur.bocage@gmail.com>
2021-03-11 17:39:41 +0100

    SignIn

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -5,12 +5,12 @@
       <label for="email">Adresse email : </label>
       <input type="email" name="email" id="email" v-model="email" required />
       <br>
-      <label for="password">Mot de passe : </label>
+      <label for="password">Mot de passe :   </label>
       <input type="password" name="password" id="password" v-model="password" required />
       <br>
       <button type="submit">Se connecter</button>
   </form>
-  Pas encore de compte ? <router-link :to="{name: 'register'}">Crées en maintement !</router-link>
+  <p>Pas encore de compte ? <router-link :to="{name: 'register'}">Crées en maintement !</router-link></p>
 </template>
 
 <script>
@@ -47,5 +47,38 @@ export default {
 </script>
 
 <style scoped>
+    form {
+        margin-left: 1%;
+        margin-top: 1%;
+        font-family: Arial, sans serif;
+    }
+    h1 {
+        margin-left: 1%;
+        font-family: Arial, sans serif;
+    }
+    p {
+        margin-left: 1%;
+        margin-top: 1%;
+        font-family: Arial, sans serif;
+    }
+    button {
+        margin-top: 1%;
+        font-family: Arial, sans serif;
+        background-color: #838383;
+        border: none;
+    }
+    input {
+        background-color: #838383;
+        border: none;
+    }
+    br {
+        margin-top: 1%;
+    }
+    a {
+        color: white;
+    }
+    a :hover {
+        color: #838383
+    }
 
 </style>
