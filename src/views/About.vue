6e9aa7a4417961c6e0607f34247405626d3f4323
Arthur <arthur.bocage@gmail.com>
2021-03-11 16:14:46 +0100

    Modif de la page about.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
-  <h1>About view component</h1>
+  <h1>A propos</h1>
+  <h2>Liste de membres du groupe</h2>
+    <div>
+        <ul>
+            <li>LETELLIER Guillaume 21804030</li>
+            <li>PIGNARD Alexandre 21701890</li>
+            <li>PIERRE Corentin 21803752</li>
+            <li>BOCAGE Arthur 21806332</li>
+        </ul>
+    </div>
+    <h2>But de ce projet</h2>
+    <p>Le but de ce projet est de créer une appliaction de gestion de tâches en Vue.js uniquement.</p>
+      <h2>Cette aplication permet d'éffectuer les tâches suivantes :</h2>
+    <div>
+        <ul>
+            <li>Créer un compte ou s’authentifier à l'API pour récupérer ses informations et un token de connexion (qui sera utiliser dans tous les autres appels API)</li>
+            <li>Créer une liste</li>
+            <li>Afficher les listes</li>
+            <li>Afficher les todos d’une liste</li>
+            <li>Créer, modifier, compléter, supprimer des todos</li>
+            <li>Filtrer l’affichage des todos</li>
+            <li>Afficher le nombre de todos non faites</li>
+        </ul>
+    </div> 
+    
 </template>
 
 <script>
